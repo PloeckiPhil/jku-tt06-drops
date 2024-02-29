3132367ffbdcf533872338085b79b0f23cc6897a
PloeckiPhil <philipp.ploeckinger@gmail.com>
2024-02-29 11:31:36 +0100

    removed parameter5

--- test/tb.v
+++ test/tb.v
@@ -26,7 +26,7 @@ module tb ();
   wire [7:0] uio_oe;
 
   // Replace tt_um_example with your module name:
-  tt_um_drops 
+  tt_um_drops
   	tt_um_drops_dut(
 
 	    // Include power ports for the Gate Level test:
