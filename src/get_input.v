2f6fea057b9c6c290d2abc39f638cc81c771723a
PloeckiPhil <philipp.ploeckinger@gmail.com>
2024-02-29 10:53:21 +0100

    removed parameter

--- src/get_input.v
+++ src/get_input.v
@@ -1,75 +1,91 @@
-/*
-	Module to catch left right input and prevent button bounce
-*/
 
-`ifndef __GETINPUT__
-`define __GETINPUT__
+`ifndef __GET_INPUT__
+`define __GET_INPUT__
 
 module get_input
 #(
- parameter cr_but = 4 // counter range to prevent button bounce
+ parameter cr = 4 // counter range
 ) (
  // define I /O ’ s of the module
  	input wire clk_i , // clock
-	input wire e_inp ,
- 	input wire rst_i , // reset
+ 	//input rst_i , // reset
+ 	input wire e_inp, // enable input
+ 	
  	input wire right_i ,
  	input wire left_i ,
-
-	output wire right_o,
-	output wire left_o,
-	output wire d_inp_o
+	
+	output wire right_o ,
+ 	output wire left_o ,
+ 	
+ 	
+ 	//output rst_o ,
+ 	
+ 	
+ 	output wire d_inp_o 
 );
 
- 	reg left;
- 	reg right;
- 	reg d_inp;
+	// start the module implementation
+	// register to store the counter value
+
+	//reg rst ; // clock
+ 	reg left ;
+ 	reg right ;
+ 	reg d_inp ;
 	
-	// counter till next input is used
-	reg [cr_but-1:0] left_cr;
-	reg [cr_but-1:0] right_cr;
+	reg [1:0] left_cr = 0;
+	reg [1:0] right_cr = 0;
+	//reg [cr-1:0] rst_cr =0 ;
 	
 	// assign the counter value to the output
 	assign left_o = left;
 	assign right_o = right;
+	//assign rst_o = rst;
 	assign d_inp_o = d_inp;
 
 	
 	always @ (posedge clk_i) begin		
-		if(rst_i) begin
+		if (e_inp) begin
+			if(left_cr == 0) begin
+				if(left_i) begin
+					left <= 1'b1;
+					left_cr <= left_cr + 1;
+				end else left <= 1'b0;
+			end else begin
+				left <= 1'b0;
+				left_cr <= left_cr + 1;
+			end
+		
+			if(right_cr == 0) begin
+				if(right_i) begin
+					right <= 1'b1;
+					right_cr <= right_cr + 1;
+				end else right <= 1'b0;
+			end else begin
+				right_cr <= right_cr + 1;
+				right <= 1'b0;
+			end
+			/*
+			if(rst_cr == 0) begin
+				if(rst_i) begin
+					rst <= 1'b1;
+					rst_cr <= 1;
+				end else rst <= 1'b0;
+			end else begin
+				rst_cr <= rst_cr + 1;
+				rst <= 1'b0;
+			end
+			*/
+			d_inp <= 1'b1;
+		end else begin
 			left <= 1'b0;
 			right <= 1'b0;
-			left_cr <= 0;
-			right_cr <= 0;
-		end else begin
-			if (e_inp) begin
-				if(left_cr == 0) begin
-					if(left_i) begin
-						left <= 1'b1;
-						left_cr <= 1;
-					end else left <= 1'b0;
-				end else begin
-					left <= 1'b0;
-					left_cr <= left_cr + 1;
-				end
-			
-				if(right_cr == 0) begin
-					if(right_i) begin
-						right <= 1'b1;
-						right_cr <= 1;
-					end else right <= 1'b0;
-				end else begin
-					right_cr <= right_cr + 1;
-					right <= 1'b0;
-				end				
-			end
+			//rst <= 1'b0;
+			d_inp <= 1'b0;
 		end
-		d_inp <= 1'b1; //always reset to prevent 2nd run
 	end
-
 				
 
 endmodule // input
 
 `endif
-`default_nettype wire
+
